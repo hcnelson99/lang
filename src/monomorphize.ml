867785f2cdcc40a6b5ddb9e4ad3eca0d843c5bec
Henry Nelson <hcnelson99@gmail.com>
2021-01-14 18:13:21 -0500

    Monomorphic cloning without specialization

--- src/monomorphize.ml
+++ src/monomorphize.ml
@@ -47,12 +47,12 @@ type ty_insts =
       ; insts : Hir.Ty.t Hir.Ty.Var.Table.t list
       }
 
+(* TODO: come up with a better name than "context" *)
 (* TODO: no real reason for this to be mutable table instead of a map other than that i'm generally lazy about threading things around *)
+type poly_insts = ty_insts Symbol.Table.t
 
-type context = ty_insts Symbol.Table.t
-
-let print_context (ctx : context) =
-  Hashtbl.iteri ctx ~f:(fun ~key ~data ->
+let print_poly_insts (poly_insts : poly_insts) =
+  Hashtbl.iteri poly_insts ~f:(fun ~key ~data ->
       print_string (Symbol.to_string key);
       print_string " -> ";
       match data with
@@ -66,16 +66,16 @@ let print_context (ctx : context) =
                   [%string "\t\t%{Hir.Ty.Var.to_string key} -> %{Hir.Ty.to_string data}"])))
 ;;
 
-let defined_as_mono ctx v = Hashtbl.add_exn ctx ~key:v ~data:Mono
+let defined_as_mono poly_insts v = Hashtbl.add_exn poly_insts ~key:v ~data:Mono
 
-let defined_as_poly ctx v ty =
+let defined_as_poly poly_insts v ty =
   if Hir.Ty.is_poly ty
-  then Hashtbl.add_exn ctx ~key:v ~data:(Poly { poly_ty = ty; insts = [] })
-  else defined_as_mono ctx v
+  then Hashtbl.add_exn poly_insts ~key:v ~data:(Poly { poly_ty = ty; insts = [] })
+  else defined_as_mono poly_insts v
 ;;
 
-let used_as ctx v ty =
-  match Hashtbl.find ctx v with
+let used_as poly_insts v ty =
+  match Hashtbl.find poly_insts v with
   | None -> failwith "ICE: var never defined"
   | Some Mono -> ()
   | Some (Poly { poly_ty; insts }) ->
@@ -83,29 +83,48 @@ let used_as ctx v ty =
     (* TODO: n^2 very slow *)
     if List.exists insts ~f:(fun x -> Hir.Ty.Var.Table.equal Hir.Ty.equal inst x)
     then ()
-    else Hashtbl.set ctx ~key:v ~data:(Poly { poly_ty; insts = inst :: insts })
+    else Hashtbl.set poly_insts ~key:v ~data:(Poly { poly_ty; insts = inst :: insts })
 ;;
 
-let rec record_polymorphic_uses (ctx : context) (ty, exp) =
-  match exp with
-  | Hir.Var v -> used_as ctx v ty
-  | Hir.Int _ -> ()
-  | Hir.Ap (e1, e2) ->
-    record_polymorphic_uses ctx e1;
-    record_polymorphic_uses ctx e2
-  | Hir.Abs (v, e) ->
-    defined_as_mono ctx v;
-    record_polymorphic_uses ctx e
+let analyze_polymorphic_uses e =
+  let rec go (poly_insts : poly_insts) (ty, exp) =
+    match exp with
+    | Hir.Var v -> used_as poly_insts v ty
+    | Hir.Int _ -> ()
+    | Hir.Ap (e1, e2) ->
+      go poly_insts e1;
+      go poly_insts e2
+    | Hir.Abs (v, e) ->
+      defined_as_mono poly_insts v;
+      go poly_insts e
+    | Hir.Let (v, e1, e2) ->
+      go poly_insts e1;
+      let t, _ = e1 in
+      defined_as_poly poly_insts v t;
+      go poly_insts e2
+  in
+  let poly_insts = Symbol.Table.create () in
+  go poly_insts e;
+  poly_insts
+;;
+
+let rec clone_lets poly_insts ((ty, e) as tyexp) =
+  match e with
+  | Hir.Var _ -> tyexp
+  | Hir.Int _ -> tyexp
+  | Hir.Ap (e1, e2) -> ty, Hir.Ap (clone_lets poly_insts e1, clone_lets poly_insts e2)
+  | Hir.Abs (v, e) -> ty, Hir.Abs (v, clone_lets poly_insts e)
   | Hir.Let (v, e1, e2) ->
-    record_polymorphic_uses ctx e1;
-    let t, _ = e1 in
-    defined_as_poly ctx v t;
-    record_polymorphic_uses ctx e2
+    (match Hashtbl.find_exn poly_insts v with
+    | Mono -> ty, Hir.Let (v, clone_lets poly_insts e1, clone_lets poly_insts e2)
+    | Poly { poly_ty = _; insts } ->
+      let e1' = clone_lets poly_insts e1 in
+      let e2' = clone_lets poly_insts e2 in
+      List.fold insts ~init:e2' ~f:(fun e2 _poly_inst -> ty, Hir.Let (v, e1', e2)))
 ;;
 
 let monomorphize e =
-  let ctx = Symbol.Table.create () in
-  record_polymorphic_uses ctx e;
-  print_context ctx;
-  failwith ""
+  let polymorphic_uses = analyze_polymorphic_uses e in
+  print_poly_insts polymorphic_uses;
+  clone_lets polymorphic_uses e
 ;;
